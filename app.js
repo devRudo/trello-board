f29171466088989ac01e32dd8e73f3bb4fd7dd56
VIJAY KUMAR MISHRA <vijay.mishra@mountblue.tech>
2020-04-02 22:27:19 +0530

    Add frontend UI for trello boards and lists - Initialize the project to use bootstrap

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const cwd = process.cwd();
-const config = require(path.join(cwd, '/config/config'));
-const viewsDirectory = path.join(cwd, '/views');
+const config = require(path.join(cwd, "/config/config"));
+const viewsDirectory = path.join(cwd, "/views");
 const port = config.port;
+const trello_api_key = config.TRELLO_API_KEY;
+const trello_token = config.TRELLO_TOKEN;
+
+const fetch = require("node-fetch");
 
 // overriding environment variable in development environment mode
 let options = {};
-if (config.NODE_ENV !== 'production') {
-    options.overrideProcessEnv = true;
-    require('dotenv-extended').load();
+if (config.NODE_ENV !== "production") {
+  options.overrideProcessEnv = true;
+  require("dotenv-extended").load();
 }
 
 // Creating new express server instance
 const app = express();
 
 // Serving static files
-app.use(express.static(path.join(cwd, '/public')));
+app.use(express.static(path.join(cwd, "/public")));
 
 // setting middleware view engine
-app.set('views', viewsDirectory);
-app.set('view engine', 'ejs');
+app.set("views", viewsDirectory);
+app.set("view engine", "ejs");
 
 // defining routes
 app.get('/', (request, response) => {
-    response.render('index');
+  response.render("index");
+});
+
+app.get('/board/:id', (request, response) => {
+  response.render('pages/board');
 });
 
 // listening to server on defined port
 app.listen(port, () => {
-    console.log(`Web app is running on ${port}`);
+  console.log(`Web app is running on ${port}`);
 });
--- config/config.js
+++ config/config.js
@@ -1,5 +1,7 @@
 require('dotenv-extended').load();
 module.exports = {
     NODE_ENV: process.env.NODE_ENV,
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    TRELLO_API_KEY: process.env.key,
+    TRELLO_TOKEN: process.env.token
 }
